0a994160978729242afebc37fc767cfd8bf9d1bc
centinelacode <centinela1024tb@gmail.com>
2022-03-09 13:13:32 -0600

    [ADD] Se agrega la configuración para configurar los stylos con css y sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
    entry: './src/index.js',
@@ -27,6 +28,14 @@ module.exports = {
                   loader: 'html-loader',
                }
             ]
+         },
+         {
+            test: /\.(css|scss)$/i,
+            use: [
+               "style-loader",
+               "css-loader",
+               "sass-loader",
+            ]
          }
       ]
    },
@@ -34,6 +43,13 @@ module.exports = {
       new HtmlWebpackPlugin({
          template: './public/index.html',
          filename: 'index.html',
+      }),
+      new MiniCssExtractPlugin({
+         filename: '[name].css'
       })
-   ]
+   ],
+   devServer: {
+      static: path.join(__dirname, 'dist'),
+      port: 3005,
+   }
 }
