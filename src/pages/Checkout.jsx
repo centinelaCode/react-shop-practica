084323209343643722bf28dedbea68215dace56f
centinelacode <centinela1024tb@gmail.com>
2022-03-10 17:22:13 -0600

    [UPD] Se modifican algunos elementos del html y css

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -19,31 +19,8 @@ const Checkout = () => {
             </p>
             <p>$560.00</p>
           </div>
-
-          <div className="shopping-cart">
-            <figure>
-              <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike" />
-            </figure>
-            <p>Bike</p>
-            <p>$30,00</p>
-          </div>
-
-          <div className="shopping-cart">
-            <figure>
-              <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike" />
-            </figure>
-            <p>Bike</p>
-            <p>$30,00</p>
-          </div>
-
-          <div className="shopping-cart">
-            <figure>
-              <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike" />
-            </figure>
-            <p>Bike</p>
-            <p>$30,00</p>
-          </div>
         </div>
+        <OrderItem />
       </div>
     </div>
   )
