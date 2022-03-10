c459c57c73c986d5386f0356fcfabb1982149819
centinelacode <centinela1024tb@gmail.com>
2022-03-09 18:06:06 -0600

    [ADD] Se agrega al reac-router-dom las diferentes rutas

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
  
 import Layout from '../containers/Layout';
-import Login from '../containers/Login';
-import RecoveryPassword from '../containers/RecoveryPassword';
 import Home from '../pages/Home';
+import Login from '../pages/Login';
+import RecoveryPassword from '../pages/RecoveryPassword';
+import SendEmail from '../pages/SendEmail';
+import NewPassword from '../pages/NewPassword';
+import MyAccount from '../pages/MyAcccount';
+import CreateAccount from '../pages/CreateAccount';
+import Checkout from '../pages/Checkout';
+import Orders from '../pages/Orders';
 import NotFound from '../pages/NotFound';
+
 import '../styles/global.css';
 
 const App = () => {
@@ -15,12 +22,14 @@ const App = () => {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
-          {/* Otra forma de renderizar componentes
-          <Route exact path="/login">
-            <Login />
-          </Route>   */}
           <Route exact path="/recovery-password" component={RecoveryPassword} />
-          <Route component={NotFound} />
+          <Route exact path="/send-email" component={SendEmail} />
+          <Route exact path="/new-password" component={NewPassword} />
+          <Route exact path="/account" component={MyAccount} />
+          <Route exact path="/signup" component={CreateAccount} />
+          <Route exact path="/checkout" component={Checkout} />
+          <Route exact path="/orders" component={Orders} />
+          <Route path="*" component={NotFound} />
         </Switch>      
       </Layout>
     </Router>
