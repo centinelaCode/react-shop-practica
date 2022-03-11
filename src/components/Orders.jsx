9ce32c69c11df122d73dee61496ab2a94c23a3d5
centinelacode <centinela1024tb@gmail.com>
2022-03-11 14:11:30 -0600

    [UPD] Se ajustan stilos y clases

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/Order.scss';
+import '../styles/Orders.scss';
 
 const Orders = () => {
   return (
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import OrderItem from '../components/OrderItem';
+import '../styles/Orders.scss';
 
 const Orders = () => {
   return (
