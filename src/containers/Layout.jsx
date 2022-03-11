490b3b2b794bd1d65d394f2395dda716e5619907
centinelacode <centinela1024tb@gmail.com>
2022-03-11 15:35:00 -0600

    [UPD] Se actualizan referencia a estilos css

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -4,7 +4,7 @@ import Header from '../components/Header';
 const Layout = ({ children }) => {
   return (
     <div className="Layout">
-      <Header />
+      {/* <Header /> */}
       { children }
     </div>
   )
