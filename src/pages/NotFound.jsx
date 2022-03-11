ab6c505236559bd4e32cf0fcb4cb3fc3bb746bce
centinelacode <centinela1024tb@gmail.com>
2022-03-11 17:54:33 -0600

    [UPD] Se agregan los alias y las img a pages/ y al App

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-import '../styles/NotFound.scss';
+import '@styles/NotFound.scss';
 
 const NotFound = () => {
   return (
