717fd3fb02578d50207704f41026dca02474ba90
centinelacode <centinela1024tb@gmail.com>
2022-03-10 18:00:35 -0600

    [ADD] Se agrega el html y css al container MyOrder

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
 
+
 const MyOrder = () => {
   return (
-    <div>MyOrder</div>
+    <aside class="MyOrder">
+      <div class="title-container">
+        <img src="./icons/flechita.svg" alt="arrow" />
+        <p class="title">My order</p>
+      </div>
+
+      <div class="my-order-content">
+        <OrderItem />
+        
+        <div class="order">
+          <p>
+            <span>Total</span>
+          </p>
+          <p>$560.00</p>
+        </div>
+
+        <button class="primary-button">
+          Checkout
+        </button>
+      </div>    
+    </aside>
   )
 }
 
