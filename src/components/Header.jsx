81f35ac8308e62be5a55bb936bbd90302726db24
centinela1024tb <centinela1024tb@gmail.com>
2022-03-18 18:47:53 -0600

    [ADD] Se actualiza el contador de productos agregados al cart asi como mostrar y ocultar MyOrder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from 'react';
 import Menu from '@components/Menu';
 import AppContext from '../context/AppContext';
+import MyOrder from '../containers/MyOrder';
 import '@styles/Header.scss';
 
 import menuIcon from '@icons/icon_menu.svg';
@@ -10,6 +11,7 @@ import shoppingCard from '@icons/icon_shopping_cart.svg';
 const Header = () => {
 
   const [toggle, setToggle] = useState(false);
+  const [toggleOrders, setToggleOrders] = useState(false);
   const { state } = useContext(AppContext);
 
   const handleToggle = () => {
@@ -51,13 +53,17 @@ const Header = () => {
             className="navbar-email"
             onClick={handleToggle}
           >platzi@example.com</li>
-          <li className="navbar-shopping-cart">
+          <li 
+            className="navbar-shopping-cart" 
+            onClick={() => setToggleOrders(!toggleOrders)}
+          >
             <img src={shoppingCard} alt="shopping cart" />
             {state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
           </li>
         </ul>
       </div>       
-      {toggle && <Menu />}     
+      {toggle && <Menu />}   
+      {toggleOrders && <MyOrder />}  
     </nav>
   )
 }
