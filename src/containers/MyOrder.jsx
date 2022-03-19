19cdbea7f919d4a51b0d40ab2af3b657665bb8f3
centinela1024tb <centinela1024tb@gmail.com>
2022-03-18 19:08:55 -0600

    [ADD] Se muestran los productos en MyOrders y se obtiene la suma total a pagar

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -10,26 +10,32 @@ const MyOrder = () => {
 
   const { state } = useContext(AppContext);
 
+  const sumTotal = () => {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue.price;
+    const sum = state.cart.reduce(reducer, 0);
+    return sum;
+  }
+
   return (
-    <aside class="MyOrder">
-      <div class="title-container">
+    <aside className="MyOrder">
+      <div className="title-container">
         <img src={arrow} alt="arrow" />
-        <p class="title">My order</p>
+        <p className="title">My order</p>
       </div>
 
-      <div class="my-order-content">
+      <div className="my-order-content">
         {state.cart.map(product => (
           <OrderItem product={product} key={`orderItem-${product.id}`} />
         ))}
         
-        <div class="order">
+        <div className="order">
           <p>
             <span>Total</span>
           </p>
-          <p>$560.00</p>
+          <p>$ {sumTotal()}</p>
         </div>
 
-        <button class="primary-button">
+        <button className="primary-button">
           Checkout
         </button>
       </div>    
